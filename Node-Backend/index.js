f98cd196c201af8496f5f355c011ba4eb4cfc72b
zakie khan <zk.khan2003@gmail.com>
2025-05-25 09:39:08 +0530

    Updating index.js

--- Node-Backend/index.js
+++ Node-Backend/index.js
@@ -12,6 +12,10 @@ app.use(express.json());
 
 app.use("/todo",todorouter);
 
+app.get("/",(req,res)=>{
+    res.send({"message":"This is the Node API"});
+})
+
 app.listen(port,()=>{
     console.log("Server Started at port :"+port);
 })
